54c3ad4afb13458cf956e0cd84f67c361b716faf
cmsandoval2 <cmsandova2@miners.utep.edu>
2023-02-17 21:25:59 -0700

    added space_char function

--- src/token.c
+++ src/token.c
@@ -1,9 +1,24 @@
+#include "tokenizer.h"
 #include <stdio.h>
 int main(){
   char str[50];
   printf("$");
   scanf("%s",str);
   puts(str);
+  char first=space_char(str);
+  if(first==1)
+    puts("There is a space");
+  else{
+    puts("No Spaces or tabs");
+  }
   
+
+}
+int space_char(char c){
+  char *a=&c;
+  while(*a++){
+    if(c==' '||c=='\t') return 1;
+  }
+  return 0;
 
 }
